6d7c74c6cc95bf4a7a057b968fe79986ca397985
GylingP <gylingpolar@outlook.com>
2024-11-15 02:22:43 +0000

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	cmd.Main()
 }
 func init() {
-	viper.SetConfigFile(".\\conf\\conf.yaml")
+	viper.SetConfigFile("./conf/conf.yaml")
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Printf("Error reading config file: %s\n", err)
 		return
@@ -24,7 +24,7 @@ func init() {
 	conf.TokenFile = viper.GetString("auth.0.tokenFile")
 	conf.Kubeconfig = viper.GetString("auth.1.kubeconfig")
 	conf.AdminCert = viper.GetString("auth.2.crt")
-	conf.AdminCert = viper.GetString("auth.2.key")
+	conf.AdminCertKey = viper.GetString("auth.2.key")
 
 	var sshConfig structure.SSHConfig
 	if err := viper.UnmarshalKey("ssh.0", &sshConfig); err != nil {
